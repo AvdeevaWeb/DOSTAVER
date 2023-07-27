57363e698b4fa5c1e18ead47293c72ae709b35e6
AvdeevaWeb <avdeeva_web@mail.ru>
2023-07-27 13:19:49 +0300

    add burger menu

---
+++ script/menu-btn.js
@@ -0,0 +1,10 @@
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.header-section__nav')
+const menuMobile = document.querySelector('.menu-btn-mobile')
+
+
+menuBtn.addEventListener('click', function(){
+	menu.classList.toggle('active')
+    menuMobile.classList.toggle('active')
+    menuBtn.classList.toggle('active')
+})
